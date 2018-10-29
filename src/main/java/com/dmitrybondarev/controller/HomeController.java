210b112fddb310cc917c42660bf37915a4f7e015
Dmitry Bondarev <tgenman@gmail.com>
2018-10-29 13:10:20 +0300

    Fix pom.xml - Move versions to <properties>

--- src/main/java/com/dmitrybondarev/controller/HomeController.java
+++ src/main/java/com/dmitrybondarev/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class MainController {
+public class HomeController {
 
     @GetMapping
     public String home() {
