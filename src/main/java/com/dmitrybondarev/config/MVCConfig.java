df9107bceca8e465402204a75b0aa7201789218b
Dmitry Bondarev <tgenman@gmail.com>
2018-10-29 13:16:58 +0300

    Move String literals to constants

--- src/main/java/com/dmitrybondarev/config/MVCConfig.java
+++ src/main/java/com/dmitrybondarev/config/MVCConfig.java
@@ -7,19 +7,20 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.dmitrybondarev")
 public class MVCConfig implements WebMvcConfigurer {
 
+    private static final String PREFIX = "/WEB-INF/view/";
+    private static final String SUFFIX = ".jsp";
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        resolver.setPrefix("/WEB-INF/view/");
-        resolver.setSuffix(".jsp");
+        resolver.setPrefix(PREFIX);
+        resolver.setSuffix(SUFFIX);
         return resolver;
     }
 }
