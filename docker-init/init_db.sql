b9d214bd33105769ceaa34db071143ada6b0be51
Dmitry Bondarev <tgenman@gmail.com>
2018-12-09 14:57:06 +0300

    Fix bug with dependencies with Lombok and attach dockers files

---
+++ docker-init/init_db.sql
@@ -0,0 +1,7 @@
+DROP SCHEMA IF EXISTS `internet_shop` ;
+
+-- -----------------------------------------------------
+-- Schema school
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `internet_shop` DEFAULT CHARACTER SET utf8 ;
+
